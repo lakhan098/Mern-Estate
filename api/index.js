4ee0e44418def050e3b71f83879fa63933c8c6a0
lakhan098 <lakhanjdh2019@gmail.com>
2023-10-18 12:57:15 +0530

    Middleware to handle error

--- api/index.js
+++ api/index.js
@@ -16,9 +16,24 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req,res) => {
+    res.send("App is running");
+});
+
 app.use("/api/user", UserRouter);
 
 app.use("/api/auth", AuthRouter);
+
+//Middleware to handle error
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+});
 
 app.listen(3000, ()=>{
     console.log("server is running on port 3000!");
