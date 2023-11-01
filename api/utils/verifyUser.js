b3bf04634075510535d0bf54a87b70f13082f637
lakhan098 <lakhanjdh2019@gmail.com>
2023-11-01 15:22:45 +0530

    update user completed

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -4,7 +4,7 @@ import { errorHandler } from "./error.js";
 export const verifyToken = (req,res,next) => {
     const token = req.cookies.access_token;
 
-    if(!token) return next(errorHandler(401,'Forbidden'));
+    if(!token) return next(errorHandler(401,'You are not Signed In'));
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if(err) return next(errorHandler(404,'Forbidden'));
